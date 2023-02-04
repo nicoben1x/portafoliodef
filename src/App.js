c3561b3136e31c0080235b31aafe01dca10fe04f
nicoben1x <88980384+nicoben1x@users.noreply.github.com>
2023-02-04 14:26:01 -0300

    cambio tamaño de algunas palabras

--- src/App.js
+++ src/App.js
@@ -33,20 +33,27 @@ function App() {
       <div className='acercademi'>
 
         <img className='bannerfoto' src='https://i.ibb.co/yfxKRcw/banerrr.png' />
-        <img className='perfilfoto' src='https://i.postimg.cc/PqJj9w2J/20221211-213641.jpg' />
-        <h1 className='nombreperfil'>Nicolás Benavidez</h1>
-        <h2 className='rolperfil'>Programador Web Fullstack Jr.</h2>
-        <p className='acercaperfil'> Soy una persona proactiva, organizada
-          y responsable, con buenas relaciones interpersonales.
-          Siempre tengo la mejor disposición para la realización de mis
-          labores. Busco un puesto de trabajo desafiante.</p>
+        <div className='fotoytext'>
+
+          <div className='textt'>
+
+
+            <img className='perfilfoto' src='https://i.postimg.cc/PqJj9w2J/20221211-213641.jpg' />
+            <h1 className='nombreperfil'>Nicolás Benavidez</h1>
+            <h2 className='rolperfil'>Programador Web Fullstack Jr.</h2>
+          </div>
+          <p className='acercaperfil'> Soy una persona proactiva, organizada
+            y responsable, con buenas relaciones interpersonales.
+            Siempre tengo la mejor disposición para la realización de mis
+            labores. Busco un puesto de trabajo desafiante.</p>
+        </div>
 
       </div>
 
-      <Educacion/>
+      <Educacion />
 
 
-    <Skills/>
+      <Skills />
 
 
 
@@ -54,7 +61,7 @@ function App() {
   );
 }
 
-function menuDes(){
+function menuDes() {
   document.querySelector(".menuDes").classList.toggle("ocultar");
 }
 
